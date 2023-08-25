9a6c3cbed7cc19b037e58e156d1b76a54c2a177c
Godfred D. Sintim-Misah <sintim.misa@gmail.com>
2023-08-25 16:46:03 +0000

    portfolio assignment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Portfolio from './components/portfolio';
 
 function App() {
   return (
     <div>
- <h1>Hello World</h1>
-   <p> I am Godfred Duah Sintim-Misah, this is my first React application.</p>
+ <h1>Portfolio</h1>
+ <Portfolio/>
+   
     </div>
    
    
---
+++ src/components/portfolio.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export function Portfolio() {
+  
+    return (
+      <div>
+        <strong>Fullname: </strong>
+        <p>Godfred Duah Sintim-Misah</p>
+        <strong>Education Qualification: </strong>
+        <p>BEd. Education - <em>1st Degree</em></p>
+        <strong>Professional Background: </strong>
+        <p>Wordpress Designer</p>
+        <strong>Skills:</strong>
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+        </ul>
+        
+      </div>
+    )
+  
+}
+
+export default Portfolio;
